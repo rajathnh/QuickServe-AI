aa0f1ce95bdf849b1004575c5d582719d6876261
Prajnan_Vaidya24 <prajnan247@gmail.com>
2025-03-21 16:18:44 +0530

    Final frontend commit ig

--- frontend/register.js
+++ frontend/register.js
@@ -33,3 +33,6 @@ document.getElementById("registerForm").addEventListener("submit", async functio
         alert("Registration failed.");
     }
 });
+function redirectToLogin() {
+    window.location.href = "login.html";
+}
